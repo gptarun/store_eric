edafc7fe0f2197a6ae9d0b734f3ff1e6fbc274a5
Tarun Gupta <tarung1201@gmail.com>
2019-11-26 13:03:15 +0530

    Added map controls and get coordinates.

--- StoreFront/src/app/map/map.component.ts
+++ StoreFront/src/app/map/map.component.ts
@@ -12,7 +12,9 @@ export class MapComponent implements OnInit {
   style = 'mapbox://styles/accordifysolutions/ck39u6rml0j1w1cs2rvnghwv0';
   lat = 37.75;
   lng = -122.41;
+
   constructor() { }
+
   ngOnInit() {
     mapboxgl.accessToken = environment.mapbox.accessToken;
     this.map = new mapboxgl.Map({
@@ -21,7 +23,28 @@ export class MapComponent implements OnInit {
       zoom: 13,
       center: [this.lng, this.lat]
     });
+
     // Add map controls
-    this.map.addControl(new mapboxgl.NavigationControl());
+    this.map.addControl(new mapboxgl.NavigationControl());    
+  }
+
+  navigateStore(longitude: any, latitude: any) {
+    this.lng = longitude;
+    this.lat = latitude;
+    console.log("clicked", longitude, " ", latitude);
+    this.map.flyTo({ center: [this.lng, this.lat] });
+
+  }
+  onRightClick(e) {
+    this.map.on('mousemove', function (e) {
+      console.log(e.point);
+      console.log(e.lngLat);
+      document.getElementById('info').innerHTML =
+        // e.point is the x, y coordinates of the mousemove event relative
+        // to the top-left corner of the map
+        JSON.stringify(e.point) + '<br />' +
+        // e.lngLat is the longitude, latitude geographical position of the event
+        JSON.stringify(e.lngLat.wrap());
+    });
   }
 }
